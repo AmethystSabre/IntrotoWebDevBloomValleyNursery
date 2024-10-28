bf84eaac3ca848ec4761299894ba42edd985636a
AmethystSabre <amethystsabre@gmail.com>
2024-10-28 01:07:10 +0000

    JS Start

---
+++ script.js
@@ -0,0 +1,9 @@
+function subscribetoNewsletter() { alert("Thank you for subscribing to our newsletter!"); }
+
+let button = document.getElementById("submit");
+button.addEventListener("click", subscribetoNewsletter);
+
+function addToCart(product)
+
+let buttontocart = document.getElementById("add-to-cart");
+button.addEventListener("click", addToCart)

5cfa73068f94365848891a78a61d634732573bb6
AmethystSabre <amethystsabre@gmail.com>
2024-11-03 23:06:08 +0000

    Shopping Cart Update

--- script.js
+++ script.js
@@ -1,9 +1,82 @@
-function subscribetoNewsletter() { alert("Thank you for subscribing to our newsletter!"); }
+window.onload = function() {
+  console.log("JavaScript Loaded");
 
-let button = document.getElementById("submit");
-button.addEventListener("click", subscribetoNewsletter);
+  function subscribetoNewsletter() { alert("Thank you for subscribing."); }
 
-function addToCart(product)
+  let subscribe = document.getElementById("submit");
+  subscribe.addEventListener("click", subscribetoNewsletter);
 
-let buttontocart = document.getElementById("add-to-cart");
-button.addEventListener("click", addToCart)
+  var modal = document.getElementById("cart");
+  var viewCart = document.getElementById("view-cart");
+  var span = document.getElementsByClassName("close")[0];
+
+  console.log("Modal:", modal);
+  console.log("View Cart Button:", viewCart);
+  console.log("Close Button:", span);
+
+  viewCart.onclick = function() {
+    console.log("View Cart Button Clicked");
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    console.log("Close Button Clicked");
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      console.log("Outside Modal Clicked");
+      modal.style.display = "none";
+    }
+  }
+
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+  function addToCart(itemId, itemName, quantity = 1) {
+    const itemIndex = cart.findIndex(item => item.id === itemId);
+
+    if (itemIndex > -1) {
+      cart[itemIndex].quantity += quantity;
+    } else {
+      cart.push({ id: itemId, name: itemName, quantity });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    console.log(`added ${quantity} of ${itemName} to cart`);
+    updateCartDisplay();
+  }
+
+  function clearCart() {
+    cart = [];
+    localStorage.removeItem('cart');
+    console.log("Cart Cleared")
+    alert("Cart Cleared")
+    updateCartDisplay();
+  }
+
+  function updateCartDisplay() {
+    const cartContainer = document.getElementById('cart');
+    cartContainer.innerHTML = '';
+
+    cart.forEach(item => {
+      const itemElement = document.createElement('div');
+      itemElement.textContent = `${item.name} - Quantity: ${item.quantity}`;
+      cartContainer.appendChild(itemElement);
+    });
+  }
+
+  updateCartDisplay();
+
+
+
+  document.querySelectorAll(".add-to-cart").forEach(buttontocart => {
+    buttontocart.addEventListener("click", () => {
+      const itemId = buttontocart.getAttribute("data-item-id");
+      const itemName = buttontocart.getAttribute("data-item-name");
+      addToCart(Number(itemId), itemName);
+      alert(`${itemName} has been added to your cart!`);
+    });
+  });
+
+};
